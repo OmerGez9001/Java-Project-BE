6aa0acd0ce9b826f7d17095464804f7d2fc1f497
itay <cl_itayb@rnd-hub.com>
2022-09-14 17:10:57 +0300

    change config

--- src/main/java/com/store/backend/config/WebSocketConfig.java
+++ src/main/java/com/store/backend/config/WebSocketConfig.java
@@ -13,12 +13,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat");
-        registry.addEndpoint("/chat").withSockJS();
+
     }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic", "/queue");
         registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
     }
 
 }
--- src/main/java/com/store/backend/controller/SocketController.java
+++ src/main/java/com/store/backend/controller/SocketController.java
@@ -3,14 +3,14 @@ package com.store.backend.controller;
 import com.store.backend.data.dto.Message;
 import com.store.backend.data.dto.WorkerDetails;
 import com.store.backend.service.SocketService;
-import lombok.AllArgsConstructor;
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
@@ -21,10 +21,12 @@ import java.security.Principal;
 
 @Controller
 public class SocketController {
-
     @Autowired
     @Lazy
     private SocketService socketService;
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
 
     @EventListener
     public void onDisconnectEvent(SessionDisconnectEvent disconnectEvent) {
@@ -40,9 +42,20 @@ public class SocketController {
 
     @MessageMapping("/chat.send")
     @SendTo("/topic/public")
-    public Message sendMessage(@Payload Message chatMessage) {
+    public Message sendGlobalMessage(@Payload Message chatMessage) {
         return chatMessage;
     }
+
+    @MessageMapping("/secured/room")
+    public void sendSpecific(
+            @Payload Message msg,
+            Principal user,
+            @Header("simpSessionId") String sessionId) throws Exception {
+
+        simpMessagingTemplate.convertAndSendToUser(
+                msg.getSender(), "queue/specific-user", msg);
+    }
+
 
     private WorkerDetails workerDetailsByPrincipal(Principal principal) {
         return (WorkerDetails) ((UsernamePasswordAuthenticationToken) principal).getDetails();
